a10931465a93e9a76f2a65bce8efb45ade09e0f9
Ralph Romero <ralph@romero.id.au>
2022-12-06 12:45:42 +1100

    day 3 complete

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+const string alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var solution1 = Day3.Resource.Input
+    .TrimEnd()
+    .Split("\r\n")
+    .Select(x => new string[] { x.Substring(0, x.Length / 2), x.Substring(x.Length / 2) })
+    .Select(x => x[0].Intersect(x[1]).First())
+    .Select(x=> alpha.IndexOf(x)+1);
+
+Console.WriteLine("Part1: " + solution1.Sum());
+
+var solution2Start = Day3.Resource.Input
+    .TrimEnd()
+    .Split("\r\n");
+
+var grouped = GetLines(solution2Start.ToArray());
+var intersectAndScore = grouped
+    .Select(x => x[0].Intersect(x[1]).Intersect(x[2]).First())
+    .Select(x => alpha.IndexOf(x) + 1);
+
+IEnumerable<string[]> GetLines(string[] inArray)
+{
+    for(int i = 0;i<inArray.Count();i=i+3)
+    {
+        yield return new string[] { inArray[i], inArray[i + 1], inArray[i + 2] };
+    }
+}
+
+Console.WriteLine("Part2: " + intersectAndScore.Sum());
