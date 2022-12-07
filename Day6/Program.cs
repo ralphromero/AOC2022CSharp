66b505e016b461ba5c5e58e58cf583897a146498
Ralph Romero <ralph@romero.id.au>
2022-12-08 10:02:40 +1100

    Day 6 complete

---
+++ Day6/Program.cs
@@ -0,0 +1,29 @@
+﻿var input = Day6.Resource.Input;
+
+int result1 = 0;
+for(int i = 4;i < input.Length;i++)
+{
+    var startIndex = i - 4;
+    var stuff = input[startIndex..i];
+    if (stuff.Distinct().Count() == 4)
+    {
+        result1 = i;
+        break;
+    }
+}
+
+Console.WriteLine("Part1: " + result1);
+
+int result2 = 0;
+for (int i = 14; i < input.Length; i++)
+{
+    var startIndex = i - 14;
+    var stuff = input[startIndex..i];
+    if (stuff.Distinct().Count() == 14)
+    {
+        result2 = i;
+        break;
+    }
+}
+
+Console.WriteLine("Part2: " + result2);
