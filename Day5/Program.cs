f2582387232facf337eb047ef2620e30b2b5e4b9
Ralph Romero <ralph@romero.id.au>
2022-12-08 08:52:50 +1100

    day 5 complete

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -60,6 +60,57 @@ for(int i = 0; i < StackCount; i++)
 
 Console.WriteLine("Part1: " + (new string(sol1)));
 
+//Part2
+
+//Init stack
+Stack<char>[] stacks2 = new Stack<char>[StackCount];
+for (int i = 0; i < StackCount; i++)
+{
+    stacks2[i] = new Stack<char>();
+}
+
+//Populate stacks
+foreach (var singleStack in StackDesign)
+{
+    int count = 0;
+    for (int i = 1; i < TotalCount; i = i + 4)
+    {
+        if (singleStack[i] != ' ')
+            stacks2[count].Push(singleStack[i]);
+        count++;
+    }
+}
+
+//Reverse all stacks because we loaded it in reverse
+for (int i = 0; i < StackCount; i++)
+{
+    stacks2[i] = new Stack<char>(stacks2[i]);
+}
+
+foreach (var movement in Movements)
+{
+    var tempStack = new Stack<char>();
+    for (int i = 0; i < movement.amount; i++)
+    {
+        tempStack.Push(stacks2[movement.fromStack - 1].Pop());
+    }
+
+    for (int i = 0; i < movement.amount; i++)
+    {
+        stacks2[movement.toStack - 1].Push(tempStack.Pop());
+    }
+}
+
+var sol2 = new char[StackCount];
+for (int i = 0; i < StackCount; i++)
+{
+    if (stacks2[i].Count() > 0)
+        sol2[i] = stacks2[i].Pop();
+}
+
+Console.WriteLine("Part2: " + (new string(sol2)));
+
+
 Directions ParseDirections(string line)
 {
     var amountMatch = Regex.Match(line, "(?<=move )\\d*");
